580f2f07b304ca5ff5558f39fa63ac701f1b7906
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2021-10-03 11:08:56 +0100

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'Univeristy@123.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "user name: $name.\n".
+                "user email: $visitor_email.\n".
+                "subject: $subject.\n".
+                "message: $message.\n".
+
+
+
+
+
+
+
+
+
+?>
